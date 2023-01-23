b87082fed4fbc42e58f492b529ae7f91512aa587
Jake Atkinson <jake.atkinson@postgrad.manchester.ac.uk>
2023-01-23 09:14:27 +0000

    feat: purple air connector

--- connectors/purple_air/purple_air_connector.py
+++ connectors/purple_air/purple_air_connector.py
@@ -1,25 +1,10 @@
-import os
-from typing import List, Dict
+import pymongo
+from connectors.purple_air.purple_air_config import PurpleAirConfig
+from typing import Generator
 
-from pydantic import BaseSettings, Field
 
-class PurpleAirConfig(BaseSettings):
-    '''
-    These configs define the structure of the data pulled from the Purple Air API.
-    API_KEY should be stored in a local .env file.
-    '''
-    API_KEY: str = Field(None, env="PA_API_KEY")
-    LATITUDE: str = Field('latitude')
-    LONGITUDE: str = Field('longitude')
-    ALTITUDE: str = Field('altitude')
-    CAN_SENSOR_INDEX: List[str] = Field(["index_1", 'index_2', 'index_3'])
-    AVERAGE_HUMIDITY: str = Field('temperature')
-    AVERAGE_TEMPERATURE: str = Field('humidity')
-    AVERAGE_PRESSURE: str = Field('pressure')
-    ESTIMATED_PM_1: str = Field('pm1.0')
-    ESTIMATED_PM_2_POINT_5: str = Field('pm2.5')
-    ESTIMATED_PM_10: str = Field('pm10.0')
-    AVERAGE_VOC: str = Field('voc')
-    OZONE: str = Field('ozone1')
-    SCATTERING_COEFF: str = Field('scattering_coefficient')
-    BATCH_SIZE = 100  # CHECK THIS
+class PurpleAirConnector:
+
+    def __init__(self):
+        self.config = PurpleAirConfig()
+        
